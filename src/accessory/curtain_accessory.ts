127564d29e7338e3081db84f978307815e11de71
zoucaiyuan <caiyuan.zou@coolkit.cn>
2023-01-04 14:48:50 +0800

    fix: curtain参数

--- src/accessory/curtain_accessory.ts
+++ src/accessory/curtain_accessory.ts
@@ -8,13 +8,16 @@ export class curtain_accessory extends base_accessory {
         online: false,
         percent: 20
     };
-    public positionState = 2;
+    public positionState = 0;
+    public targetPercent = 0;
 
     service: Service | undefined;
 
     constructor(platform: HomebridgePlatform, accessory: PlatformAccessory | undefined, device: IDevice) {
         super(platform, accessory, Categories.WINDOW_COVERING, device);
         this.state = this.initDeviceState(this.state, this.device);
+        this.positionState = this.state.percent! > 0 ? 1 : 2;
+        this.targetPercent = this.state.percent!;
         this.platform.log.info('curtain_accessory------>', this.state.online, this.state.percent);
     }
     mountService(): void {
@@ -23,10 +26,17 @@ export class curtain_accessory extends base_accessory {
         this.service?.getCharacteristic(this.platform.Characteristic.PositionState).onGet(() => this.positionState);
         this.service
             ?.getCharacteristic(this.platform.Characteristic.TargetPosition)
-            .onGet(() => this.state.percent!)
+            .onGet(() => this.targetPercent)
             .onSet((value: CharacteristicValue) => {
-                this.state.percent = value as number;
-                this.platform.log.info('--->', value);
+                this.targetPercent = value as number;
+                this.platform.log.info('position:', this.state.percent, '===>', value);
+                // 调用接口
+                setTimeout(() => {
+                    this.state.percent = value as number;
+                    this.positionState = 2;
+                    this.service?.updateCharacteristic(this.platform.Characteristic.CurrentPosition, this.state.percent);
+                    this.service?.updateCharacteristic(this.platform.Characteristic.PositionState, this.positionState);
+                }, 2000)
             });
     }
 }
