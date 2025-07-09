50210a22f2064eae6f46292d531f25f6ea5f634d
alexsaidani <alex@ayslabs.co>
2025-07-10 05:39:54 +0700

    naming config updates

--- src/config/platformConfig.ts
+++ src/config/platformConfig.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'eWeLinkCUBEPlatform';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-plugin-ewelink-cube';
+export const PLUGIN_NAME = 'homebridge-plugin-ewelink-cube-multi-gang-support';
